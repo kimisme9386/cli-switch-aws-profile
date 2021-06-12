87c7a85c988eea8f9d01832e8e6e1e5719b58566
Chris Yang <kimisme9386@gmail.com>
2021-06-13 00:40:29 +0800

    feat: add assume role

---
+++ assume-role.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ASSUME_ROLE_NAME=$1
+sts_session_name=chris-mac-desktop
+
+accountid=$(aws sts get-caller-identity | jq -r .Account)
+temp_role=$(aws sts assume-role --role-arn arn:aws:iam::${accountid}:role/${ASSUME_ROLE_NAME} --role-session-name ${sts_session_name} --duration-seconds 28800)
+AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
+AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
+AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
+
+if [ -z ${AWS_ACCESS_KEY_ID} ]; then
+  exit 1
+fi
+
+echo 'aws_access_key_id = '${AWS_ACCESS_KEY_ID}
+echo 'aws_secret_access_key = '${AWS_SECRET_ACCESS_KEY}
+echo 'aws_session_token = '${AWS_SESSION_TOKEN}
--- switch_profile/switch_profile.py
+++ switch_profile/switch_profile.py
@@ -2,15 +2,20 @@
 
 import pathlib
 import re
+import subprocess
 import sys
+from typing import Tuple
 
 from PyInquirer import prompt
 from examples import custom_style_2
 
-# credential_file = f"{pathlib.Path.home()}/.aws/credentials"
-credential_file = f"{pathlib.Path(__file__).parents[1]}/credentials_sample"
+credential_file = f"{pathlib.Path.home()}/.aws/credentials"
+# credential_file = f"{pathlib.Path(__file__).parents[1]}/credentials_sample"
 profile_default_title = "[default]"
 profile_default_title_name = "default"
+
+assume_role_key = 'custom_assume_role'
+assume_role_profile_name = '[custom_assume_role]'
 
 
 def validate_aws_profile(credential_file: str):
@@ -61,6 +66,50 @@ def write_aws_profiles(profiles: dict) -> None:
             file.write("".join(item))
 
 
+def execute_assume_role(role: str) -> Tuple[str, bool]:
+    bash_file = f"{pathlib.Path(__file__).parents[1]}/assume-role.sh"
+    process = subprocess.Popen(['sh', bash_file, role],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if stderr:
+        return stderr.decode("utf-8"), True
+
+    return stdout.decode("utf-8"), False
+
+
+def get_selected_profile_role_name(profile_content: list) -> str:
+    for content in profile_content:
+        if content.find("=") == -1:
+            continue
+        key, value = content.split("=")
+        if str(key).strip() == assume_role_key:
+            return str(value).strip()
+
+    return ''
+
+
+def add_assume_role_default_profile(
+        profiles: dict,
+        profile_default_title: str,
+        assume_role_profile_name: str,
+        assume_role_profile_content: str
+) -> None:
+    # Remove origin default profile title and do uncomment
+    for profile, item in profiles.items():
+        if item[1].find(profile_default_title) != -1 and item[0] == f";{assume_role_profile_name}":
+            del profiles[profile]
+
+        elif item[1].find(profile_default_title) != -1 and item[0][0] == ";":
+            profiles[profile][0] = profiles[profile][0][1:]
+            del profiles[profile][1:2]
+
+    # Append assume role profile title to file
+    content = [f";{assume_role_profile_name}\n", f"{profile_default_title}\n", assume_role_profile_content]
+    profiles.update({assume_role_profile_name: content})
+
+
 if __name__ == '__main__':
     validate_aws_profile(credential_file)
     profiles = get_aws_profiles(credential_file)
@@ -89,13 +138,28 @@ if __name__ == '__main__':
     ]
 
     answers = prompt(questions, style=custom_style_2)
+    is_assume_role = answers.get('assume_role')
 
     profile_selected = answers.get('profile')
 
     if profiles[profile_selected][1].find(profile_default_title) != -1:
-        print('Selected profile is default already.')
-        sys.exit(0)
+        if is_assume_role is False:
+            print('Selected profile is default already.')
+            sys.exit(0)
+    else:
+        change_default_profile(profiles, profile_selected, profile_default_title)
+        write_aws_profiles(profiles)
 
-    change_default_profile(profiles, profile_selected, profile_default_title)
+    if is_assume_role:
+        role_name = (answers.get('role_name') if answers.get('role_name')
+                     else get_selected_profile_role_name(profiles[profile_selected]))
 
-    write_aws_profiles(profiles)
+        output, err = execute_assume_role(role_name)
+        if err:
+            print(output)
+            sys.exit(0)
+
+        add_assume_role_default_profile(profiles, profile_default_title, assume_role_profile_name, output)
+        write_aws_profiles(profiles)
+
+        print('Use "aws sts get-caller-identity" to identify who you are.\n')
