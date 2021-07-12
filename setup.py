c0dd5bf100a6b520eaf5ba9243b912abdb3825b3
Chris Yang <kimisme9386@gmail.com>
2021-07-12 16:33:12 +0800

    feat: add setting for pypi

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='SwitchAWSProfile',
+    version='0.0.1',
+    author='Chris Yang',
+    author_email='kimisme9386@gmail.com',
+    license='MIT',
+    description='Switch AWS profile on local',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kimisme9386/cli-switch-aws-profile',
+    py_modules=['switch_profile'],
+    packages=find_packages(),
+    install_requires=['pyinquirer==1.0.3'],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points='''
+        [console_scripts]
+        awssp=switch_profile.app:main
+    '''
+)
